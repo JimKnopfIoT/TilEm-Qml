762e6cfe083101169acafbb8a493e395c7c1ea52
labsin <sam.sgrs@gmail.com>
2016-08-23 22:06:44 +0200

    Fix CMake

--- app/manifest.json.cmake
+++ app/manifest.json.cmake
@@ -1,12 +1,11 @@
 {
     "architecture": "amd64",
     "description": "TilEm is an Texas Instruments calculator emulator",
-    "framework": "ubuntu-sdk-14.04-papi-dev1",
+    "framework": "ubuntu-sdk-15.04",
     "hooks": {
         "TilEm": {
-            "apparmor": "TilEm.json",
-            "desktop": "TilEm.desktop",
-            "urls": "TilEm.url-dispatcher"
+            "apparmor": "TilEm.apparmor",
+            "desktop": "TilEm.desktop"
         }
     },
     "maintainer": "Sam Segers <sam.sgrs@gmail.com>",
--- cmake/Modules/QtCreatorResources.cmake
+++ cmake/Modules/QtCreatorResources.cmake
@@ -1,3 +1,3 @@
-macro( qtcreator_add_project_resources resources )
+macro( qtcreator_add_project_resources)
   add_custom_target( ${PROJECT_NAME}_Resources ALL SOURCES ${ARGN} )
 endmacro()
